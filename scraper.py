2a4d3705974585fc32504ac41ba8f1ae16dcec52
danny.white <daniel.j.white7@navy.mil>
2020-07-19 16:42:25 -0500

    cleaning up a bit

--- scraper.py
+++ scraper.py
@@ -21,31 +21,13 @@ def main():
 
             for goodquestion in goodquestions:
                 cleanedquestions = goodquestion.replace("&#8217;", "")
-                #for cleanquestion in cleanedquestions:
                 if '+000' in cleanedquestions:
                     pass
                 elif '[' in cleanedquestions:
                     pass
                 else:
                     print cleanedquestions
-
-
-                # if '&#8217;' in goodquestion:
-                #     #cleangoodquestion = re.finditer(r'(.*)&#8217;(.*)',goodquestion)
-                #     #print cleangoodquestion
-                #     for cleanedquestion in cleangoodquestion:
-                #         cleanedquestion.relace('&#8217;', "'")
-                #         #print cleanedquestion.group(1)
-                #         #print cleanedquestion.group(2)
-                # else:
-                #     # if '+000' in goodquestion:
-                #     #     pass
-                #     # elif '[' in goodquestion:
-                #     #     pass
-                #     # else:
-                #     #     print goodquestion
-                #     pass
-
+                    
         except Exception, e:
             print str(e)
 
